a98e84d8a18b6781832d00e98c20a2867a22d9ef
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-01-20 20:23:58 -0800

    implemented the ability to write a new task and altered css

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -5,7 +5,8 @@ import Task from './Task.jsx';
 const Title = styled.h1`
     font-family: Arial;
     text-align: center;
-    font-size: 1em;
+    font-size: 2em;
+    text-decoration: underline;
 `;
 
 const List = styled.div`
@@ -17,10 +18,36 @@ const List = styled.div`
 
 const Delete = styled.button`
     border-radius: 5px;
+    border: 1px solid transparent;
     background-color: red;
     color: white;
     &:hover {
         cursor: pointer;
+        border: 1px solid white;
+    }
+`;
+
+const Input = styled.textarea`
+    height: auto;
+    font-family: Arial;
+    background-color: #F4F4F4;
+    border-radius: 5px;
+    font-size: 1em;
+    box-shadow: 10px 10px 20px gray;
+    padding: 10px;
+`;
+
+const Button = styled.button`
+    border-radius: 5px;
+    width: auto;
+    height: fit-content;
+    border: 1px solid transparent;
+    background-color: #F4F4F4;
+    box-shadow: 10px 10px 20px gray;
+    z-index: 2;
+    &:hover {
+        cursor: pointer;
+        border: 1px solid white;
     }
 `;
 
@@ -33,10 +60,13 @@ class TaskList extends React.Component {
                 {task: 'take out the trash', completed: false, deleted: false},
                 {task: 'feed the cat', completed: false, deleted: false},
                 {task: 'love Suzzy', completed: false, deleted: false}
-            ]
+            ],
+            newTask: ''
         }
 
         this.delete = this.delete.bind(this);
+        this.saveTask = this.saveTask.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     delete(e) {
@@ -49,11 +79,36 @@ class TaskList extends React.Component {
                 break;
             }
         }
-        console.log(index);
         var newState = this.state.tasks;
         newState[index].deleted = true;
         this.setState({
             tasks: newState
+        });
+    }
+
+    saveTask() {
+        if (this.state.newTask === '') {
+            return;
+        }
+        var input = document.getElementById('input');
+        input.value = '';
+        var newTask = {
+            completed: false,
+            deleted: false
+        };
+        newTask.task = this.state.newTask;
+        var newState = this.state.tasks;
+        newState.push(newTask);
+        this.setState({
+            tasks: newState,
+            newTask: ''
+        });
+    }
+
+    handleChange(e) {
+        var text = e.target.value;
+        this.setState({
+            newTask: text
         });
     }
 
@@ -70,6 +125,8 @@ class TaskList extends React.Component {
         });
         return (
             <>
+                <Input onChange={this.handleChange} id="input" rows={5} placeholder="What do you need to do?"/>
+                <Button onClick={this.saveTask}>Save</Button>
                 <Title>Task List</Title>
                 <List>{tasks}</List>
             </>
